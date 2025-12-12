f636cbfdfa91adece157cbda5ba6c6547d8a709c
zesanahmed <zesanahmed593@gmail.com>
2025-12-12 20:12:57 +0600

    basic project setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  // 🔥 IGNORE BLOCK (put this first)
+  {
+    ignores: ['node_modules/*', 'dist/*'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+
+  tseslint.configs.recommended,
+
+  {
+    rules: {
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+  },
+  prettierRecommended,
+]);
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+
+  res.send(a);
+});
+
+export default app;
